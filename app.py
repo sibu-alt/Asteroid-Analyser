b96ecccdc8ae2c741cfd5e172add85f4c501387e
Sibusisiwe <sibusisiwekhumalo156@gmail.com>
2026-05-11 23:31:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -491,8 +491,8 @@ def discover_asteroids():
         return jsonify({'success': False, 'error': f'Error discovering asteroids: {str(e)}'})
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+#if __name__ == '__main__':
+    #app.run(debug=True, port=5000)
 
 
 
